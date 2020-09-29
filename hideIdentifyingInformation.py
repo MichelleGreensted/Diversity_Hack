329d0c9858f976f11c63a7e6df9cbdddbefcc7f5
simonaantonova <58565460+simonaantonova@users.noreply.github.com>
2020-09-30 02:45:53 +0300

    Create hideIdentifyingInformation.py

---
+++ hideIdentifyingInformation.py
@@ -0,0 +1,12 @@
+def find_Index (word, file_name):  #function takes in the identifying word and the file that contain the resume as an argument
+    with open(file_name) as f:     
+        for i, line in enumerate (f, 1):
+            if word in line: 
+                return i        #the function returns the index of the line in which the identifying word has occured 
+
+def hide_Entire_Line (i, file_name):
+    lines = open(file_name).read().splitlines() #open file, 
+    lines[i] = '***********************'
+    open('out.txt', "w").write('\n'.join(lines))
+
+hide_Entire_Line(find_Index("Name", "sample.txt"), "sample.txt")
