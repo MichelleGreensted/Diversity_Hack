cc16bd9f6838b9eda5d1e51a75238c54a353202e
jewelkoh <67817166+jewelkoh@users.noreply.github.com>
2020-10-01 21:24:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,20 @@ function myFunction1() {
       moreText.style.display = "inline";
     }
   }
+
+
+      function myFunction3() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more3");
+        var btnText = document.getElementById("myBtn3");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "Read more";
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = "Read less";
+          moreText.style.display = "inline";
+        }
+      }
